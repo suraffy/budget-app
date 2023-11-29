dbc997c8d3b2c1bb8741d5b7faba770a3439ab77
Surafel Araya <surafelay@gmail.com>
2023-11-29 21:23:44 +0300

    Set state for budget analysis

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,25 @@
+import { useState } from "react";
 import Navbar from "./common/Navbar";
 
 const Home = () => {
+  const currency = "$";
+
+  const [currentBudget, setCurrentBudget] = useState(0);
+  const [totalIncome, setTotalIncome] = useState(0);
+  const [expenses, setExpenses] = useState(0);
+
   return (
     <div className="container">
       <Navbar />
 
       <div className="flex flex-col items-center p-4">
         <div className="font-medium">
-          <h4>Total Income: $500</h4>
-          <h4>Expenses: $219</h4>
+          <h4>Total Income: {currency + totalIncome}</h4>
+          <h4>Expenses: {currency + expenses}</h4>
         </div>
-        <h2 className="font-semibold text-2xl">Available Budget: $230</h2>
+        <h2 className="font-semibold text-2xl">
+          Available Budget: {currency + currentBudget}
+        </h2>
       </div>
     </div>
   );
