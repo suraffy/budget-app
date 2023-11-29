9e435778d718d19be6355ab1d30453d8ea7d9810
Surafel Araya <surafelay@gmail.com>
2023-11-29 20:58:14 +0300

    Build a Header

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,16 @@ import Navbar from "./common/Navbar";
 
 const Home = () => {
   return (
-    <div className="container ">
+    <div className="container">
       <Navbar />
+
+      <div className="flex flex-col items-center p-4">
+        <div className="font-medium">
+          <h4>Total Income: $500</h4>
+          <h4>Expenses: $219</h4>
+        </div>
+        <h2 className="font-semibold text-2xl">Available Budget: $230</h2>
+      </div>
     </div>
   );
 };
