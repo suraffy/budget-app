7ce076314f3adb88b4aeab6154f2bd694cdccb21
Surafel Araya <surafelay@gmail.com>
2023-11-29 20:41:07 +0300

    Container Default Centering & Horizontal Padding

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    container: {
+      center: true,
+      padding: "1rem",
+    },
   },
   plugins: [],
 };
