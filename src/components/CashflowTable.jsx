a469167c594cc5e202119d62bdbd289dd3ac01f6
Surafel Araya <surafelay@gmail.com>
2023-12-01 18:59:07 +0300

    Retouch CashflowTable component

--- src/components/CashflowTable.jsx
+++ src/components/CashflowTable.jsx
@@ -2,8 +2,8 @@ import CashflowItem from "./CashflowItem";
 
 const CashflowTable = ({ items, currency, onDelete }) => {
   return (
-    <div className="mt-8">
-      <div className="flex justify-between flex-wrap">
+    <div className="mt-8 p-4 pb-6 shadow-lg border border-slate-300 rounded-lg">
+      <div className="flex justify-between flex-wrap mb-2">
         <h4 className="w-1/3 text-lg font-bold">Reason</h4>
         <h4 className="w-1/3 text-lg font-bold">Amount</h4>
         <span className="w-1/4"></span>
--- src/components/common/PieChart.jsx
+++ src/components/common/PieChart.jsx
@@ -2,6 +2,16 @@ import { Chartjs } from "chart.js/auto";
 import { Pie } from "react-chartjs-2";
 
 const PieChart = ({ data }) => {
+  const newData = { ...data };
+  newData.datasets[0].backgroundColor = [
+    "#FF6384",
+    "#36A2EB",
+    "#FFCE56",
+    "#8C54FF",
+    "#FF8543",
+  ];
+  newData.datasets[0].borderWidth = 4;
+
   return <Pie data={data} />;
 };
 
