43857270b59c4452ecdf2283ff5a26edb8ee544b
Surafel Araya <surafelay@gmail.com>
2023-12-01 11:38:00 +0300

    Retoching Budget Items

--- src/components/Budget.jsx
+++ src/components/Budget.jsx
@@ -1,13 +1,13 @@
 const Budget = ({ currency, availableBudget, totalIncome, totalExpense }) => {
   return (
-    <div className="flex flex-col items-center p-4">
+    <div className="flex flex-wrap gap-2 sm:gap-6 justify-center items-center p-4">
+      <h2 className="font-semibold text-2xl">
+        Available Budget: {currency + availableBudget}
+      </h2>
       <div className="font-medium">
         <h4>Total Income: {currency + totalIncome}</h4>
         <h4>Expenses: {currency + totalExpense}</h4>
       </div>
-      <h2 className="font-semibold text-2xl">
-        Available Budget: {currency + availableBudget}
-      </h2>
     </div>
   );
 };
