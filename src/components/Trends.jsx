9d081682543496c62f07a70af28ff248eac769c1
Surafel Araya <surafelay@gmail.com>
2023-12-02 13:33:17 +0300

    Set unavailable message

--- src/components/Trends.jsx
+++ src/components/Trends.jsx
@@ -1,11 +1,15 @@
+import { incomeItemsList, expenseItemsList } from "../services/cashflowData";
+
 const Trends = () => {
   return (
     <div className="mb-32">
       <h2 className="text-4xl font-semibold text-center">
-        This Year Cashflow Trends
+        Annual Cashflow Trends
       </h2>
 
-      <div className=""></div>
+      <div className="my-10 text-center">
+        <p className="text-lg">There is no enought data is available!</p>
+      </div>
     </div>
   );
 };
