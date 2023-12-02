93a73d50bcaae168b785ebf3cd1779b41c3cd006
Surafel Araya <surafelay@gmail.com>
2023-12-02 10:48:57 +0300

    Add Planned Cashflow Data

---
+++ src/services/plannedCashflowData.js
@@ -0,0 +1,54 @@
+const plannedIncomeList = [
+  {
+    cashflow: "income",
+    year: 2023,
+    month: 11,
+    reason: "Salary",
+    amount: 5000,
+  },
+  {
+    cashflow: "income",
+    year: 2023,
+    month: 11,
+    reason: "Side Project",
+    amount: 2000,
+  },
+  {
+    cashflow: "income",
+    year: 2023,
+    month: 11,
+    reason: "Blog Ads",
+    amount: 400,
+  },
+];
+
+const plannedExpenseList = [
+  {
+    cashflow: "expense",
+    year: 2023,
+    month: 12,
+    reason: "House Rent",
+    amount: 600,
+  },
+  {
+    cashflow: "expense",
+    year: 2023,
+    month: 12,
+    reason: "Grocery",
+    amount: 150,
+  },
+  {
+    cashflow: "expense",
+    year: 2023,
+    month: 12,
+    reason: "Restaurant",
+    amount: 50,
+  },
+  {
+    cashflow: "expense",
+    year: 2023,
+    month: 12,
+    reason: "Internet",
+    amount: 60,
+  },
+];
