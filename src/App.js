5e12d56b3bcaab9530231c45fc83968e969ae64f
Surafel Araya <surafelay@gmail.com>
2023-12-02 15:05:57 +0300

    Build Footer Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Home from "./components/Home";
 import Insight from "./components/Insight";
 import Trends from "./components/Trends";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Home />
       <Insight />
       <Trends />
+      <Footer />
     </div>
   );
 }
