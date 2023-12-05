45aec017d23da8be392ef399075b9146d23c1810
Surafel Araya <surafelay@gmail.com>
2023-12-06 02:30:32 +0300

    Retouch Buttons

--- src/components/common/TryItConfirmationModal.jsx
+++ src/components/common/TryItConfirmationModal.jsx
@@ -33,7 +33,7 @@ const TryItConfirmationModal = ({ showModal, onCloseModal, onTryIt }) => {
         />
         <div className="flex justify-end">
           <button
-            className="mr-2 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 leading-5"
+            className="mr-2 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 leading-5"
             onClick={handleButtonClick}
           >
             Create
